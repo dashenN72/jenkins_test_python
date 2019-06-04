aaf5e10ac784a6c0d919c9b2d550038b29e37c49
dashenN72 <547889165@qq.com>
2019-06-04 16:56:25 +0000

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-# coding:utf-8
-import datetime
-content = datetime.datetime.now() 
-with open('read.txt', 'a', encoding='utf-8') as f:
-    print(content)
-    f.write(str(content) + '\n')
-	print('写入成功')
